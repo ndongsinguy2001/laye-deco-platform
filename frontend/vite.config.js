e28533d84a992e275f3c6d6687c3a866dd792d01
ndongsinguy2001 <ndongsinguy2001@gmail.com>
2026-07-10 09:33:52 +0000

    Fix: version simplifiée de vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          icons: ['react-icons'],
-          charts: ['recharts'],
-          utils: ['axios', 'jspdf', 'xlsx']
-        }
-      }
-    }
-  }
+  plugins: [react()]
 })
